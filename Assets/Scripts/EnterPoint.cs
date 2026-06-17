aba1d1119232ff10c541ceb9f0fccd97ba065ce6
denzmov <denzmov@gmail.com>
2026-06-17 14:44:04 +0200

    Коммит 2 тест и апдейт

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,15 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    // тестовый комментарий
+
+
+/*/
+ * wdwd
+ * 
+ * 
+ * 
+/*/
     void Start()
     {
         Time.timeScale = _timeScale;
